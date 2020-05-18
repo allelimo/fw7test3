6dfa93b338b770a844fa4230e55472c1287d9fef
alessandro limonta <allelimo@gmail.com>
2020-05-19 00:05:15 +0200

    tentativi di memorizzare il tema

--- src/js/app.js
+++ src/js/app.js
@@ -21,8 +21,45 @@ var app = new Framework7({
   name: 'fwTest3', // App name
   theme: 'auto', // Automatic theme detection
 
+    on: {
+        pageInit: function () {
+            console.log('pageInit');
+            pippo();
 
+         //   helloWorld(); // this gives me error Can’t find variable: helloWorld
+
+        }
+    },
 
   // App routes
   routes: routes,
 });
+
+function pippo() {
+
+    var globalTheme = (window.localStorage.getItem("dark") );
+
+    console.log("called pippo");
+    if (window.localStorage.getItem("dark") == "dark") {
+
+        window.localStorage.setItem("dark", "light");
+        console.log("off dark");
+
+        //var self = this;
+        //var $html = self.$('html');
+        //globalTheme = "dark";
+        //$html.removeClass('theme-dark theme-light').addClass('theme-' + globalTheme);
+        //self.$setState({ theme: globalTheme });
+    } else {
+
+        window.localStorage.setItem("dark", "dark");
+        console.log("on dark");
+
+        //var self = this;
+        //var $html = self.$('html');
+        //globalTheme = "light";
+        //$html.removeClass('theme-dark theme-light').addClass('theme-' + globalTheme);
+        //self.$setState({ theme: globalTheme });
+
+    }
+}
