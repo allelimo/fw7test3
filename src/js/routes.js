359c80934d6b65667195081ad3fbe706e7c3a435
alessandro limonta <allelimo@gmail.com>
2020-05-18 15:40:45 +0200

    Add project files.

---
+++ src/js/routes.js
@@ -0,0 +1,21 @@
+
+import HomePage from '../pages/home.f7.html';
+import AboutPage from '../pages/about.f7.html';
+import NotFoundPage from '../pages/404.f7.html';
+
+var routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/about/',
+    component: AboutPage,
+  },
+  {
+    path: '(.*)',
+    component: NotFoundPage,
+  },
+];
+
+export default routes;
