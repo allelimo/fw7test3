0d45b367103aea7422dd0a421cef211617fc56ce
alessandro limonta <allelimo@gmail.com>
2020-05-19 18:49:12 +0200

    aggiunto: pagina blank inizio form per pagina principale

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,16 +2,21 @@
 import HomePage from '../pages/home.f7.html';
 import AboutPage from '../pages/about.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
+import BlankPage from '../pages/blank.page.f7.html';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
-  },
+    },
+  {
+    path: '/blankpage/',
+    component: BlankPage,
+    },
   {
     path: '/about/',
     component: AboutPage,
-  },
+    },
   {
     path: '(.*)',
     component: NotFoundPage,
